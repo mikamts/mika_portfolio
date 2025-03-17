1d8953b151afc696b663b1021692280ec1934db6
mikamts <mikaelajoymatias@gmail.com>
2025-03-17 01:21:31 -0500

    added link for github and linkedin in contact page. Needs more beef and UI improvement

--- mikaelamatias_portfolio/src/pages/Contact.js
+++ mikaelamatias_portfolio/src/pages/Contact.js
@@ -1,3 +1,14 @@
 export default function Contact() {
-    return <h1>Contact</h1>
+    return (
+        <>
+        <h1>Contact</h1>
+
+        <ul>
+                <a href="https://www.linkedin.com/in/mikaelamatias/">LinkedIn</a>
+        </ul>
+        <ul>
+                <a href="https://github.com/mikamts">Github</a>
+        </ul>
+        </>
+    )
 }
